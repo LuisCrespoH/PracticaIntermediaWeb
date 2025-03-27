bd89f152aa2289c9429d3d72bcc645087f6e0705
LuisCrespoH <luiscrespoherrera@gmail.com>
2025-03-27 11:22:03 +0100

    Login Previo a añadir el codigo de 6 digitos

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors")
 require('dotenv').config();
 
-//const routers = require('./routes')
+const routers = require('./routes')
 const app = express()
 const dbConnect = require('./config/mongo')
 //Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
 app.use(cors())
 app.use(express.json())
-//app.use('/api', routers)
+app.use('/api', routers)
 //app.use(express.static("storage"))
 const port = process.env.PORT || 3000
 app.listen(port, () => {
